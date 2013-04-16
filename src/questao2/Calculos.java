f1d37e89110170b43b7ae55a076703411d3187ed
Leonardo Danieli <leonardo.danieli+git@gmail.com>
2013-04-16 11:12:43 -0300

    Q2 Cálculos

--- src/questao2/Calculos.java
+++ src/questao2/Calculos.java
@@ -5,10 +5,17 @@ public class Calculos {
 	/**
 	 * @param args
 	 */
+
 	public static void main(String v[]) {
 
-
-
+		System.out.println("Cálculos");
+		int a = Integer.parseInt(v[0]);
+		int b = 1;
+		while (b < a) {
+			System.out.printf("[%d %d]", a, b);
+			a++;
+			b = b * 2;
+		}
 	}
 
 }
