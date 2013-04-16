a2c9eb64def9ffdb6ff087313430be1876d4ffd4
Leonardo Danieli <leonardo.danieli+git@gmail.com>
2013-04-16 10:22:42 -0300

    finalizado produto especial Q3

--- src/questao3/ProdutoEspecial.java
+++ src/questao3/ProdutoEspecial.java
@@ -10,15 +10,22 @@ public class ProdutoEspecial extends Produto {
 
 	public void calcula() {
 
-		valorTaxas = valorBase  * 0.8;
+		valorTaxas = valorBase  * 0.08;
+
+		if(valorTaxas < 143.00){
+			valorTaxas = 143.00;
+		}
 
 		valorFinal = valorBase + valorTaxas;
-
+		System.out.println("valorTaxas:" +valorTaxas);
+		System.out.println("valorFinal:" +valorFinal);
 		if(valorFinal>4000){
 			valorFinal= valorFinal  * 1.03;
 			valorTaxas = valorFinal - valorBase;
 
 		}
+		System.out.println("valorTaxas:" +valorTaxas);
+		System.out.println("valorFinal:" +valorFinal);
 	}
 
 }
